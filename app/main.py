9f79e5e09957b9690767d39d2e5f2df59cd07a9f
Daniel Andrade Suarez <d.andrades@uniandes.edu.co>
2025-06-04 10:57:40 -0500

    Creamos carpeta api con archivos .py de los endpoints

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from app.api import register, auth, users
 #Creamos app
 app = FastAPI()
 
-#Endpoints
+#Registro de endpoints
 app.include_router(register.router, prefix = '/register')
 app.include_router(auth.router, prefix = '/auth')
 app.include_router(users.router, prefix = '/users')
